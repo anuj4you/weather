9f8ab6e616ebe1b005a30cda7a9a003eab1d5912
Anuj <anujk.581@gmail.com>
2023-01-08 22:53:07 -0500

    added all required fields

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
+import React from 'react';
 
 
+// api initialize --------
+//  example-> https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
-function App() {
-  return (
-    <div>
-             
-        <p>
-          Weather App - live interactive weather app!
-        </p>
-        
-    </div>
-  );
+const api={
+  key: "954e7fdad02b2614a54118fd952d9064",
+  base: ""
 }
 
-export default App;
-  
+// today date --------------
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+
+
+
+export default function App() {
+  return (
+    <div className='app'>
+    
+    <main>
+      <div className="searchBar">
+        <input type="text" name="city" id="searchText" placeholder='Type city name here' />
+        <button className='searchButton'>search</button>
+      </div>
+
+      <div className="cityInfo">
+        <h3>Toronto</h3>
+        <p>{today}</p>
+      </div>
+
+      <div className="results">
+        <div className="temp">current temp ...</div>
+        <div className="info"><p>Clouds...</p>
+        <p>feels like</p></div>
+        <div className="logo">logo</div>
+      </div>
+    </main>
+    
+    </div>
+  )
+}
