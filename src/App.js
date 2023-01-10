9da72b3843e73abdfc8cb7f168baaf55b3111217
Anuj <anujk.581@gmail.com>
2023-01-10 06:12:00 -0500

    final version full funictional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 // api initialize --------
 //  example-> https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 const api = {
   key: "954e7fdad02b2614a54118fd952d9064",
-  base: "",
+  base: "https://api.openweathermap.org/data/2.5/",
 };
 
 // get today's date --------------
@@ -15,77 +15,124 @@ var dd = String(today.getDate()).padStart(2, "0");
 var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
 var yyyy = today.getFullYear();
 
-const weekday = ["Sun","Mon","Tue","Wed","Thur","Fri","Sat"];
+const weekday = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
 
 const d = new Date();
 let day = weekday[d.getDay()];
 
-today = day + " " + mm + " " + dd + " " + yyyy;
+today = dd + " " + mm + " " + yyyy + ", " + day;
 
 // get image from static file
-var cloud = require('./assets/cloud.png')
-var fav = require('./assets/faviconAnuj.ico')
+
+var fav = require("./assets/faviconAnuj.ico");
 
 // App start
 export default function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  // const icon = weather.weather[0].icon;
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
   return (
     <div>
-      <div className="backgroundMain">
+      <div
+        className={
+          typeof weather.main != "undefined"
+            ? weather.main.temp > 12
+              ? "backgroundMainWarm"
+              : "backgroundMain"
+            : "backgroundMain"
+        }
+      >
         <div className="main">
           <h2 className="h2">☁️ Weather Forecast ☁️</h2>
           <div className="outputArea">
             <div className="searchBar">
-              <input type="text" name="city" placeholder="City name " />
+              <input
+                type="text"
+                name="city"
+                placeholder="City name "
+                onChange={(e) => setQuery(e.target.value)}
+                value={query}
+                onKeyPress={search}
+              />
             </div>
 
-            <div className="results">
-              <div className="result1">
-                <div className="cityInfo">
-                  <h3 className="boldFont">Toronto</h3>
-                  <p style={{fontSize: "1.2rem"}}>{today}</p>
-                </div>
-                <div className="info">
-                  <div className="temp">
-                    <h1 className="h1">5 °C</h1>
-                    <h2 className="h2">Clouds</h2>
+            {typeof weather.main != "undefined" ? (
+              <div className="results">
+                <div className="result1">
+                  <div className="cityInfo">
+                    <h3 className="boldFont">
+                      {weather.name}, {weather.sys.country}
+                    </h3>
+                    <p style={{ fontSize: "1.2rem" }}>{today}</p>
                   </div>
+                  <div className="info">
+                    <div className="temp">
+                      <h1 className="h1">{Math.round(weather.main.temp)} °C</h1>
+                      <h2 className="h2">{weather.weather[0].main}</h2>
+                    </div>
 
-                  <div className="logo">
-                  <img src={cloud} style={{width: "9em"}} alt="clouds"/>
+                    <div className="logo">
+                      <img
+                        src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                        style={{ width: "9em" }}
+                        alt="icon.."
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="result2">
+                  <div className="detail">
+                    <p>Feels like</p>
+                    <div className="value">
+                      <p className="yellow">{weather.main.feels_like} °C</p>
+                    </div>
+                  </div>
+                  <div className="detail">
+                    <p>Wind speed</p>
+                    <div className="value">
+                      <p className="yellow">{weather.wind.speed} kph</p>
+                    </div>
+                  </div>
+                  <div className="detail">
+                    <p>Humidity</p>
+                    <div className="value">
+                      <p className="yellow">{weather.main.humidity} %</p>
+                    </div>
+                  </div>
+                  <div className="detail">
+                    <p>Max Temp.</p>
+                    <div className="value">
+                      <p className="yellow">{weather.main.temp_max} °C</p>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="result2">
-                <div className="detail">
-                  <p>Feels like</p>
-                  <div className="value">
-                    <p className="yellow">5 °C</p>
-                  </div>
-                </div>
-                <div className="detail">
-                  <p>Wind speed</p>
-                  <div className="value">
-                    <p className="yellow">12 kph</p>
-                  </div>
-                </div>
-                <div className="detail">
-                  <p>Humidity</p>
-                  <div className="value">
-                    <p className="yellow">30%</p>
-                  </div>
-                </div>
-                <div className="detail">
-                  <p>Max Temp.</p>
-                  <div className="value">
-                    <p className="yellow">8 °C</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+            ) : (
+              ""
+            )}
             <div className="cityInfo">
-                  
-                  <p style={{fontSize: "0.8rem"}}>© {new Date().getFullYear()} weatherSick, Anuj Kumar Inc. <img src={fav} style={{width: "1em", marginBottom: "-2px"}} alt="fav"/> </p>
-                </div>
+              <p style={{ fontSize: "0.8rem" }}>
+                © {new Date().getFullYear()} weatherSick, Anuj Kumar Inc.{" "}
+                <img
+                  src={fav}
+                  style={{ width: "1em", marginBottom: "-2px" }}
+                  alt="fav"
+                />{" "}
+              </p>
+            </div>
           </div>
         </div>
       </div>
